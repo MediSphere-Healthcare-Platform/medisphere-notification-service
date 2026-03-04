9d0a9f9ff62e6674136164271e4a96c26a9b9c79
AroshXR <aroshanasandeep454@gmail.com>
2026-03-04 16:16:01 +0530

    initial commit

---
+++ src/main/java/com/medisphere/notification/MedisphereNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medisphere.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisphereNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisphereNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medisphere/notification/MedisphereNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisphere.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisphereNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
